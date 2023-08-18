47baf4dde4f13301fa30d4aca34c0a1405ffb715
Dmitry Brashkin <Brashkin.dmitrii@gmail.com>
2023-08-18 04:53:27 +0300

    feat: add event on click for pagination and arrows in slider

--- library/script.js
+++ library/script.js
@@ -2,12 +2,12 @@ const leftArrow = document.querySelector("#arrowLeft");
 const rightArrow = document.querySelector("#arrowRight");
 const imagesAbout = document.querySelectorAll(".imagesAbout img");
 const slider = document.querySelector(".imagesAbout");
-const itemWidth = document.querySelector(".imagesAbout img").clientWidth;
-const pagination = document.querySelectorAll(".carousel span");
-const secondPag = document.querySelector("#slideSecond");
-const busts = document.querySelector("#busts");
+const pagination = document.querySelectorAll(".slide");
+const paginationCenter = document.querySelectorAll(".slide span");
 
-let i = 0;
+let sliderCount = 0;
+let sliderWidth;
+let itemWidth = document.querySelector(".imagesAbout img").offsetWidth;
 
 /*const slideNext = function () {
     imagesAbout[i].style.display = "none";
@@ -48,7 +48,7 @@ rightArrow.onclick = function (){
     imagesAbout[i].style.display = "block";
 }*/
 
-leftArrow.addEventListener("click", scrollToPrevItem);
+/*leftArrow.addEventListener("click", scrollToPrevItem);
 rightArrow.addEventListener("click", scrollToNextItem);
 pagination[i].addEventListener("click", scrollToItem);
 
@@ -63,6 +63,88 @@ function scrollToNextItem() {
 function scrollToItem() {
     imagesAbout[i].scrollIntoView({behavior: "smooth"});
     pagination[i].classList.toggle("active");
-}
+}*/
 //Найти индекс массива передаваемого элемета пагинации и вписать его сюда как i
 //Убрать ссылки из пагинации в html
+window.addEventListener("resize", showSlide);
+
+leftArrow.addEventListener("click", prevSlide);
+rightArrow.addEventListener("click", nextSlide);
+
+function nextSlide() {
+    sliderCount++;
+    rollSlider();
+    thisSlide(sliderCount);
+    if(sliderCount === imagesAbout.length - 1) {
+        leftArrow.classList.remove("arrowDisabled");
+        rightArrow.classList.add("arrowDisabled");
+    } else {
+        rightArrow.classList.remove("arrowDisabled");
+        leftArrow.classList.remove("arrowDisabled");
+    }
+}
+
+
+function prevSlide() {
+    sliderCount--;
+    rollSlider();
+    thisSlide(sliderCount);
+    if(sliderCount === 0) {
+        leftArrow.classList.add("arrowDisabled");
+        rightArrow.classList.remove("arrowDisabled");
+    } else {
+        leftArrow.classList.remove("arrowDisabled");
+        rightArrow.classList.remove("arrowDisabled");
+    }
+}
+
+function showSlide() {
+    if(window.innerWidth <= 1024) {
+        sliderWidth = document.querySelector(".slider").offsetWidth;
+        slider.style.width = sliderWidth * imagesAbout.length + "px";
+        imagesAbout.forEach(item => item.style.width = sliderWidth + "px");
+        console.log(window.outerWidth);
+    } else {
+        sliderWidth = document.querySelector(".slider").offsetWidth;
+        slider.style.width = sliderWidth * imagesAbout.length + "px";
+        imagesAbout.forEach(item => item.style.width = (sliderWidth - 50) / 3 + "px");
+        console.log(window.innerWidth);
+    }
+    rollSlider();
+}
+
+showSlide();
+
+function rollSlider() {
+    if(window.innerWidth <= 1024) {
+        slider.style.transform = `translateX(${-sliderCount * sliderWidth}px)`
+    } else if(window.innerWidth >= 1024.02) {
+        slider.style.transform = `translateX(${-sliderCount * (((sliderWidth - 50) / 3) + 25)}px)`
+    }
+}
+
+function thisSlide(index) {
+    paginationCenter.forEach(item => item.classList.remove("activePag"));
+    pagination.forEach(item => item.classList.remove("activePag"));
+    paginationCenter[index].classList.add("activePag");
+    pagination[index].classList.add("activePag");
+}
+
+pagination.forEach((point, index) => {
+    point.addEventListener("click", () => {
+        sliderCount = index;
+        rollSlider();
+        thisSlide(sliderCount);
+        console.log(sliderCount);
+        if(sliderCount === 0) {
+            rightArrow.classList.remove("arrowDisabled");
+            leftArrow.classList.add("arrowDisabled");
+        } else if(sliderCount === imagesAbout.length - 1) {
+            leftArrow.classList.remove("arrowDisabled");
+            rightArrow.classList.add("arrowDisabled");
+        } else {
+            leftArrow.classList.remove("arrowDisabled");
+            rightArrow.classList.remove("arrowDisabled");
+        }
+    })
+})
