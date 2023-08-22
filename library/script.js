a47070ce2a5a2f4bc298f93a8ec9b023a7bddf4a
Dmitry Brashkin <Brashkin.dmitrii@gmail.com>
2023-08-22 05:06:47 +0300

    feat: add actives on click for logotype and register and login buttons

--- library/script.js
+++ library/script.js
@@ -12,6 +12,108 @@ const winterBooks = document.querySelector(".winterBooks");
 const springBooks = document.querySelector(".springBooks");
 const summerBooks = document.querySelector(".summerBooks");
 const autumnBooks = document.querySelector(".autumnBooks");
+const signUpButton = document.querySelector("#signUp");
+const logInButton = document.querySelector("#logIn");
+const logInWindow = document.querySelector(".logInWindow");
+const registerWindow = document.querySelector(".registerWindow");
+const background = document.querySelector(".background");
+const logotypeIcon = document.querySelector("#logotype");
+const profileWindowLogIn = document.querySelector(".profileWindowLogIn");
+const profileWindowNotLogIn = document.querySelector(".profileWindowNotLogIn");
+const profileRegisterButton = document.querySelector("#profileRegister");
+const profileLogInButton = document.querySelector("#profileLogIn");
+const profileLogOutButton = document.querySelector("#profileLogOut");
+const profileMyProfileButton = document.querySelector("#profileMyProfile");
+const crossIcons = document.querySelectorAll(".cross img");
+const hamburgerIcon = document.querySelector("#hamburgerIcon");
+const registerLink = document.querySelector("#registerLink");
+const logInLink = document.querySelector("#logInLink");
+const logoText = document.querySelector("#logoText");
+
+/*const text = function() {
+    logoText.textContent = "JD";
+}
+
+logoText.addEventListener("mouseover", text);*/
+
+const closeWindowNotLogIn = function() {
+    body.classList.remove("scrollBlock");
+    profileWindowNotLogIn.classList.remove("profileOpen");
+}
+
+const closeWindowLogIn = function() {
+    body.classList.remove("scrollBlock");
+    profileWindowLogIn.classList.remove("profileOpen");
+}
+
+const closeWindow = function() {
+    background.classList.remove("open");
+    logInWindow.classList.remove("open");
+    registerWindow.classList.remove("open");
+    background.classList.remove("open");
+}
+
+hamburgerIcon.addEventListener("click", closeWindowNotLogIn);
+
+logotypeIcon.addEventListener("click", function (e) {
+    e.stopPropagation();
+    body.classList.toggle("scrollBlock");
+    profileWindowNotLogIn.classList.toggle("profileOpen");
+});
+
+profileRegisterButton.addEventListener("click", function() {
+    closeWindowNotLogIn();
+    registerWindow.classList.add("open");
+    background.classList.add("open");
+});
+
+registerLink.addEventListener("click", function() {
+    closeWindow();
+    registerWindow.classList.add("open");
+    background.classList.add("open");
+});
+
+logInLink.addEventListener("click", function() {
+    closeWindow();
+    logInWindow.classList.add("open");
+    background.classList.add("open");
+});
+
+profileLogInButton.addEventListener("click", function() {
+    closeWindowNotLogIn();
+    logInWindow.classList.add("open");
+    background.classList.add("open");
+});
+
+background.addEventListener("click", closeWindow);
+
+crossIcons.forEach((crossIcon) => {
+    crossIcon.addEventListener("click", closeWindow)
+});
+
+document.addEventListener("click", function (e) {
+    const target = e.target;
+    const its_profileWindow = target == profileWindowNotLogIn || profileWindowNotLogIn.contains(target);
+    const its_logotypeIcon = target == logotypeIcon;
+    const profileWindow_is_active = profileWindowNotLogIn.classList.contains("profileOpen");
+
+    if (!its_profileWindow && !its_logotypeIcon && profileWindow_is_active) {
+        body.classList.remove("scrollBlock");
+        profileWindowNotLogIn.classList.remove("profileOpen");
+    }
+});
+
+document.addEventListener("click", function (e) {
+    const target = e.target;
+    const its_profileWindow = target == profileWindowLogIn || profileWindowLogIn.contains(target);
+    const its_logotypeIcon = target == logotypeIcon;
+    const profileWindow_is_active = profileWindowLogIn.classList.contains("profileOpen");
+
+    if (!its_profileWindow && !its_logotypeIcon && profileWindow_is_active) {
+        body.classList.remove("scrollBlock");
+        profileWindowLogIn.classList.remove("profileOpen");
+    }
+});
 
 let sliderCount = 0;
 let sliderWidth;
@@ -21,6 +123,10 @@ window.addEventListener("resize", showSlide);
 
 leftArrow.addEventListener("click", prevSlide);
 rightArrow.addEventListener("click", nextSlide);
+
+signUpButton.addEventListener("click", function() {
+
+});
 
 let winterTimeOut;
 
@@ -78,8 +184,6 @@ autumnButton.addEventListener("click", function() {
     }, 1300);
 })
 
-
-
 function nextSlide() {
     sliderCount++;
     rollSlider();
@@ -92,7 +196,6 @@ function nextSlide() {
         leftArrow.classList.remove("arrowDisabled");
     }
 }
-
 
 function prevSlide() {
     sliderCount--;
